11587450131ec49a5682d10bba7683aba90af02a
Zhn <zhukov.ever@gmail.com>
2015-09-19 12:09:07 +0500

    init: project

---
+++ instagram-popularTests/instagram_popularTests.swift
@@ -0,0 +1,36 @@
+//
+//  instagram_popularTests.swift
+//  instagram-popularTests
+//
+//  Created by Zhn on 19/09/2015.
+//  Copyright (c) 2015 zhn. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class instagram_popularTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
